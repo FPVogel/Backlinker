8eaead0d53ab8d1c53e29c68aa3ab3574e9fc9fe
FPVogel <merlinglander@vogelhosting.de>
2024-03-04 00:42:33 +0100

    make it multi-threaded

--- backlinker.py
+++ backlinker.py
@@ -1,29 +1,40 @@
-import json,requests,re,sys
+import json
+import requests
+import re
+import sys
+from concurrent.futures import ThreadPoolExecutor
+
+def check_backlink(backlink, site):
+    url = backlink['url'].replace("h4link.duckdns.org", site)
+    try:
+        r = requests.get(url).status_code
+    except KeyboardInterrupt:
+        sys.exit()
+    except:
+        r = "time out"
+    print(site + " => Backlink Eklendi ==> " + re.search('http:\/\/.*?\/', url).group(0).replace("/", "").replace("http:", "") + " status: " + str(r))
 
 try:
-  print("""
-            _ ____             _    _ _       _             
-           | |  _ \           | |  | (_)     | |            
- _   _ _ __| | |_) | __ _  ___| | _| |_ _ __ | | _____ _ __ 
+    print("""
+            _ ____             _    _ _       _
+           | |  _ \           | |  | (_)     | |
+ _   _ _ __| | |_) | __ _  ___| | _| |_ _ __ | | _____ _ __
 | | | | '__| |  _ < / _` |/ __| |/ / | | '_ \| |/ / _ \ '__|
-| |_| | |  | | |_) | (_| | (__|   <| | | | | |   <  __/ |   
- \__,_|_|  |_|____/ \__,_|\___|_|\_\_|_|_| |_|_|\_\___|_|   
-                                              H4-cklinker - wmdark.com     
+| |_| | |  | | |_) | (_| | (__|   <| | | | | |   <  __/ |
+ \__,_|_|  |_|____/ \__,_|\___|_|\_\_|_|_| |_|_|\_\___|_|
+                                              H4-cklinker - wmdark.com
   """)
-  if (sys.version_info.major == 3):
-    site = input(" => Backlink Kasilcak Site\t: ")
-  else:
-    site = raw_input(" => Backlink Kasilcak Site\t: ")
-  with open("urlbacklinks.json", "r") as file:
-    data = json.loads(file.read())
-    for backlink in data:
-      url = backlink['url'].replace("h4link.duckdns.org", site)
-      try:
-        r = requests.get(url).status_code
-      except KeyboardInterrupt:
-        sys.exit()
-      except:
-        r = "time out"
-      print(site + " => Backlink Eklendi ==> "+re.search('http:\/\/.*?\/', url).group(0).replace("/", "").replace("http:","") + " status: "+str(r))
-except:
-  print("\n\n => exit\n")
+    if sys.version_info.major == 3:
+        site = input(" => Backlink Kasilcak Site\t: ")
+    else:
+        site = raw_input(" => Backlink Kasilcak Site\t: ")
+
+    with open("urlbacklinks.json", "r") as file:
+        data = json.loads(file.read())
+        with ThreadPoolExecutor() as executor:
+            executor.map(lambda backlink: check_backlink(backlink, site), data)
+
+except Exception as e:
+    print("\n\n => exit\n")
+    print(e)
+
